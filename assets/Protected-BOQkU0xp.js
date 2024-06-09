d8d89f0ac7202a0f9545f2146797be824c8a932f
cilecar <mrcraboytm@gmail.com>
2024-06-09 15:37:19 +0300

    Updates

--- assets/Protected-BOQkU0xp.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,m as a,c as t,t as o,a as c,b as n,o as s}from"./index-DyU8RPob.js";const _={computed:{...a(["getUser"]),user(){return this.getUser}}},d=n("h1",null,"Protected Page",-1),u={key:0};function m(l,p,i,f,h,e){return s(),t("div",null,[d,e.user?(s(),t("p",u,"Welcome, "+o(e.user.name),1)):c("",!0)])}const k=r(_,[["render",m]]);export{k as default};
---
+++ assets/Protected-DwWYP5iC.js
@@ -0,0 +1,1 @@
+import{_ as r,m as a,c as t,t as o,a as c,b as n,o as s}from"./index-CssaEWop.js";const _={computed:{...a(["getUser"]),user(){return this.getUser}}},d=n("h1",null,"Protected Page",-1),u={key:0};function m(l,p,i,f,h,e){return s(),t("div",null,[d,e.user?(s(),t("p",u,"Welcome, "+o(e.user.name),1)):c("",!0)])}const k=r(_,[["render",m]]);export{k as default};
